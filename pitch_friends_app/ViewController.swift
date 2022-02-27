489fe1196e73ef8765558e8d21b05096f5c53d9a
dfvalencia <dfvalenc@bancolombia.com.co>
2022-02-27 08:06:33 -0500

    Initial Commit

---
+++ pitch_friends_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pitch_friends_app
+//
+//  Created by Daniel Felipe Valencia Rodriguez on 27/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
